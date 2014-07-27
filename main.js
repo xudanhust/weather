1ac6c54e98ae6fe2ace4bbc1925cdc79ae108e6f
xudanhust <xudanhust@gmail.com>
2014-07-28 00:50:00 +0800

    拆分模块，加入单元测试

--- main.js
+++ main.js
@@ -1,45 +1,10 @@
-var fs = require('fs'),
-    iconv = require('iconv-lite'),
-    request = require('./lib/request');
+var getCityCode = require('./lib/citycode'),
+    getCityName = require('./lib/cityname'),
+    getWeather = require('./lib/weather');
 
-var weatherUrl = 'http://www.weather.com.cn/data/cityinfo/',
-    // ipUrl = 'http://curlmyip.com/',
-    ipUrl = 'http://int.dpool.sina.com.cn/iplookup/iplookup.php';
-
-var data = JSON.parse(fs.readFileSync('data/weathercity.code.json', 'utf-8')),
-    cityName = process.argv.splice(2)[0],
-    cityCode;
-
-if(!cityName){
-    request(ipUrl, function(obj){
-        var result = iconv.decode(obj, 'GBK');
-        cityName = result.split('\t')[5];
-        printWeather(cityName);
-    });
-}else{
-    printWeather(cityName);
-}
-
-function printWeather(cityName){
-    cityCode = data[cityName];
-    if(!cityCode){
-        for(var i in data){
-            if(i.indexOf(cityName) !== -1){
-                cityCode = data[i];
-                break;
-            }
-        }
-    }
-    if(!cityCode){
-        console.error('ERROR: 找不到城市');
-        return;
-    }
-    weatherUrl += cityCode + '.html';
-    request(weatherUrl, function(obj){
-        var wData = JSON.parse(obj.toString()),
-            o = wData.weatherinfo;
+getCityName(process.argv.splice(2)[0], function(cityName){
+    getWeather(getCityCode(cityName), function(o){
+        var o = o.weatherinfo;
         console.log(o.city + '今天天气：' + o.weather + ', ' + o.temp2 + '到' + o.temp1 );
     });
-}
-
-module.exports.printWeather = printWeather;
+});
---
+++ test/test.js
@@ -0,0 +1,44 @@
+var assert = require('assert'),
+    getCityCode = require('../lib/citycode'),
+    getCityName = require('../lib/cityname'),
+    getWeather = require('../lib/weather');
+
+describe('module: getCityName', function(){
+    it('输入城市名，返回城市名', function(done){
+        getCityName('杭州', function(cityName){
+            if(cityName === '杭州'){
+                done();
+            }
+        });
+    });
+    it('不输入城市名，根据IP获取城市名', function(done){
+        getCityName(null, function(cityName){
+            if(typeof cityName === 'string'){
+                done();
+            }
+        });
+    });
+});
+
+describe('module: getCityCode', function(){
+    it('直接匹配获取城市代码', function(){
+        assert.equal('101010100', getCityCode('北京'));
+        assert.equal('101210101', getCityCode('浙江杭州'));
+    });
+    it('模糊匹配获取城市代码', function(){
+        assert.equal('101210101', getCityCode('杭州'));
+        assert.equal('101200101', getCityCode('武汉'));
+        assert.equal('101180301', getCityCode('新乡'));
+    })
+});
+
+describe('module: getWeather', function(){
+    it('输入城市代码，返回天气数据', function(done){
+        getWeather('101210101', function(o){
+            if(typeof o === 'object' && o.weatherinfo && o.weatherinfo.weather){
+                done();
+            }
+        });
+    });
+});
+
