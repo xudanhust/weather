8888210a2a72918665f0dce08b3e544c2d3556e2
xudanhust <xudanhust@gmail.com>
2014-07-27 14:10:56 +0800

    增加功能：根据IP获取城市名

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
 var fs = require('fs'),
+    iconv = require('iconv-lite'),
+    // geoip = require('geoip-lite'),
     request = require('./request');
+
+var weatherUrl = 'http://www.weather.com.cn/data/cityinfo/',
+    // ipUrl = 'http://curlmyip.com/',
+    ipUrl = 'http://int.dpool.sina.com.cn/iplookup/iplookup.php';
 
 var data = JSON.parse(fs.readFileSync('weathercity.code.json', 'utf-8')),
     cityName = process.argv.splice(2)[0],
-    cityCode = data[cityName];
+    cityCode;
 
-if(!cityCode){
-    for(var i in data){
-        if(i.indexOf(cityName) !== -1){
-            cityCode = data[i];
-            break;
-        }
-    }
+if(!cityName){
+    request(ipUrl, function(obj){
+        var result = iconv.decode(obj, 'GBK');
+        cityName = result.split('\t')[5];
+        outPutWeather(cityName);
+    });
+}else{
+    outPutWeather(cityName);
 }
 
-request(cityCode, function(obj){
-    var wData = JSON.parse(obj.toString()),
-        o = wData.weatherinfo;
-    console.log(o.city + '的天气：' + o.weather + ', ' + o.temp2 + '到' + o.temp1 );
-});
-
+function outPutWeather(cityName){
+    cityCode = data[cityName];
+    if(!cityCode){
+        for(var i in data){
+            if(i.indexOf(cityName) !== -1){
+                cityCode = data[i];
+                break;
+            }
+        }
+    }
+    if(!cityCode){
+        console.log('ERROR: 找不到城市');
+        return;
+    }
+    weatherUrl += cityCode + '.html';
+    request(weatherUrl, function(obj){
+        var wData = JSON.parse(obj.toString()),
+            o = wData.weatherinfo;
+        console.log(o.city + '今天天气：' + o.weather + ', ' + o.temp2 + '到' + o.temp1 );
+    });
+}
